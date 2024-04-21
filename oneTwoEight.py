4ee9fa6c6b1687c54fd6d914284055ace0648888
EC2 Default User <ec2-user@ip-10-16-10-74.us-west-2.compute.internal>
2024-04-21 20:56:23 +0000

    commit

--- oneTwoEight.py
+++ oneTwoEight.py
@@ -1,6 +1,17 @@
 
 import os
 import subprocess
-# os.system("ls")
+os.system("ls")
 subprocess.run(["ls"])
 subprocess.run(["ls","-l"])
+subprocess.run(["ls","-l","README.md"])
+
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
